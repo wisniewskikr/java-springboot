da3314dc73931d5717f52f2d79a4984696a40cb5
Wisniewski <wisniewk@emea.roche.com>
2022-03-23 09:53:28 +0100

    Update project springboot-docker-config-external

--- springboot-docker-config-external/src/main/java/com/example/controllers/greeting/GreetingController.java
+++ springboot-docker-config-external/src/main/java/com/example/controllers/greeting/GreetingController.java
@@ -1,36 +1,18 @@
 package com.example.controllers.greeting;
 
-import java.io.InputStream;
-import java.util.Properties;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-	private static final String CONFIGS_PROPERTIES_PATH = "configs/greeting.properties";
+	@Value(value = "${message:}")
+	private String message;
 
 	@RequestMapping(value="/")
-	public String greeting() {
-		Properties props = getPropertiesFromConfig();
-		String message = props.getProperty("message") != null ? props.getProperty("message") : "Can not read message from configuration!";
-		return message;		
-	}
-	
-	private Properties getPropertiesFromConfig() {
-		
-		Properties properties = new Properties();
-		
-		try {
-			InputStream stream = this.getClass().getClassLoader().getResourceAsStream(CONFIGS_PROPERTIES_PATH);
-			properties.load(stream);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return properties;
-		
+	public String greeting() {		
+		return !"".equals(message) ? message : "Can not read message from configuration!";
 	}
 	
 }
